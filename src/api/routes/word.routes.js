66648c46d9e4fab63fb7a7dac047d593360c9a02
Rodrigo <rodri2.papi@gmail.com>
2023-09-01 18:28:36 +0200

    endpoint word ok

--- src/api/routes/word.routes.js
+++ src/api/routes/word.routes.js
@@ -1,20 +1,17 @@
 const express = require("express");
 const wordRouter = express.Router();
 
-const { RAE } = require("rae-api");
-
-const rae = new RAE();
-
 wordRouter.get("/:palabra", async (req, res) => {
   const { palabra } = req.params;
 
-  const search = await rae.searchWord(palabra);
-  const result = search.results;
-  console.log(search.results);
-  if (search.results.length != 0) {
-    return res.status(200).json({ palabra: "true" });
+  const call = await fetch(`https://dle.rae.es/${palabra}`);
+  const text = await call.text();
+  const wordExist = text.includes("Aviso:");
+  console.log(wordExist);
+  if (wordExist) {
+    return res.status(404).json({ resultado: "No existe" });
   } else {
-    return res.status(404).json({ palabra: "false" });
+    return res.status(200).json({ resultado: "Si existe" });
   }
 });
 
